66b28693b526f8206a7a003d612fd0a6485a86d5
Connor Burton <connorburton44@gmail.com>
2018-04-06 15:18:26 +0100

    Finalise footer.php

--- CleanBuild/footer.php
+++ CleanBuild/footer.php
@@ -1,25 +1,29 @@
 			<footer class="footer">
 				<div class="container">
+
 					<div class="top-footer table">
+
 						<div class="links td vt">
-							<div class="menu-col">
-								<h2>Information</h2>
+							<div class="menu-col vt">
+								<h3>Information</h3>
 								<?php wp_nav_menu(array('menu' => 'Footer Information', 'container' => false)); ?>
 							</div>
-							<div class="menu-col">
-								<h2>Products</h2>
+							<div class="menu-col vt">
+								<h3>Products</h3>
 								<?php wp_nav_menu(array('menu' => 'Footer Products', 'container' => false)); ?>
 							</div>
 						</div>
+
 						<div class="address td vt">
 							<?php include 'inc/content/contact-details.php'; ?>
 						</div>
+
 					</div>
-				</div>
-				<div class="bottom-footer">
-					<div class="container">
-						<p>&copy; Copyright <?php echo date('Y'); ?>  <?php echo do_shortcode('[company-name]'); ?> <span>|</span> Reg no. <?php echo do_shortcode('[reg-number]') ?> <span>|</span> <a href="/terms-conditions" title="View the sites terms and conditions">Website terms and privacy policy</a></p>
+
+					<div class="bottom-footer">
+						<p>&copy; Copyright <?php echo date('Y'); ?>  <?php echo do_shortcode('[company-name]'); ?> <span>|</span> Company no. <?php echo do_shortcode('[reg-number]') ?> <span>|</span> <a href="/terms-conditions" title="View the sites terms and conditions">Website terms and privacy policy</a></p>
 					</div>
+
 				</div>
 			</footer>
 
