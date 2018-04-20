771f64ba9bf9d84f83868092928c8fa577b059c8
Connor Burton <connorburton44@gmail.com>
2018-04-20 09:34:30 +0100

    Revert "Minor Tweaks and Updates"

--- CleanBuild/functions/wordpress/wordpress-custom.php
+++ CleanBuild/functions/wordpress/wordpress-custom.php
@@ -70,11 +70,23 @@ function yoasttobottom() {
 }
 add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
 
+
+// DISABLE SRCSET ON EMBEDDED IMAGES
+function cb_disable_srcset( $sources ) {
+  return false;
+}
+add_filter( 'wp_calculate_image_srcset', 'cb_disable_srcset' );
+
+
 // HAS CHILDREN FUNCTION
 function has_children() {
   global $post;
   $children = get_pages( array( 'child_of' => $post->ID ) );
-  return $children ? true : false;
+  if( count( $children ) == 0 ) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 ?>
