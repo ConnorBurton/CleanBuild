3f2d24e73885c6a135e498f6811bfff58a27fee9
Connor Burton <connorburton44@gmail.com>
2018-04-20 10:41:24 +0100

    Moved acf options page under Theme Options + css mixin amend

--- CleanBuild/functions/acf/acf-functions.php
+++ CleanBuild/functions/acf/acf-functions.php
@@ -2,27 +2,41 @@
 
 // OPTION PAGES
 if( function_exists('acf_add_options_page') ) {
+  $theme_options_args = array(
+    'page_title' => 'Theme Options',
+    'menu_title' => 'Theme Options',
+    'menu_slug' 	=> 'theme_options',
+    'icon_url' => 'dashicons-admin-generic',
+  );
+
   $company_details_args = array(
     'page_title' => 'Company Details',
     'menu_title' => 'Company Details',
     'icon_url' => 'dashicons-id',
+    'parent_slug' 	=> 'theme_options',
     'post_id' => 'company',
   );
+
   $footer_sections_args = array(
     'page_title' => 'Footer Sections',
     'menu_title' => 'Footer Sections',
     'icon_url' => 'dashicons-editor-kitchensink',
+    'parent_slug' 	=> 'theme_options',
     'post_id' => 'footer',
   );
+
   $post_sections_args = array(
     'page_title' => 'Post Sections',
     'menu_title' => 'Post Sections',
     'icon_url' => 'dashicons-welcome-write-blog',
+    'parent_slug' 	=> 'theme_options',
     'post_id' => 'post_sections',
   );
+
+  acf_add_options_page($theme_options_args);
   acf_add_options_page($company_details_args);
-  acf_add_options_page($footer_sections_args);
-  acf_add_options_page($post_sections_args);
+  acf_add_options_sub_page($footer_sections_args);
+  acf_add_options_sub_page($post_sections_args);
 }
 
 ?>
