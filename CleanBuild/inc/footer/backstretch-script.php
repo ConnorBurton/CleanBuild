55449077dc567ea48e60d212a81bde6729cb1ff1
Jade Sperrin <jade.sperrin@ascotgroup.co.uk>
2018-10-03 16:39:37 +0100

    Enable ability to call banner_images from News Page and Custom post types

--- CleanBuild/inc/footer/backstretch-script.php
+++ CleanBuild/inc/footer/backstretch-script.php
@@ -1,4 +1,26 @@
 <script type="text/javascript">
+	
+<?php
+	
+	//Set up $option variable so we can grab banner_images from the news and other custom post types too.
+	global $post;
+	
+	//Get post ID by defualt
+	$option = $post->ID;
+	
+	//if news pages get page for posts 
+	if ( is_home() || is_single() || is_category() ) {
+		$option = get_option('page_for_posts');
+	} 
+	
+	// You can extend this to work for custom post types too e.g.
+	/*		
+	  if ( is_singular('cases') || is_post_type_archive('cases') ){
+		 $option = 'cases';
+		}
+	*/
+	
+?>
 
 var $ = jQuery.noConflict();
 
@@ -6,8 +28,8 @@ $(document).ready(function(){
 
 	$('.slider').backstretch([
     <?php
-      if(get_field('banner_images')) {
-        $images = get_field('banner_images');
+      if(get_field('banner_images', $option)) {
+        $images = get_field('banner_images', $option);
         foreach($images as $img) {
           $img = $img['url'];
   				echo  '"'. $img .'",';
